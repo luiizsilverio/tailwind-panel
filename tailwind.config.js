9cfa0109e4a8f7402ae2328b8c65b2110f09302a
luiizsilverio <luiiz.silverio@gmail.com>
2022-03-25 21:07:57 -0300

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false, // or "class" or "media"
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
